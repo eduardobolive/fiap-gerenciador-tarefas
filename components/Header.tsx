5b725d56cb5a0411e0a463fd247c832dd33e21c7
Eduardo Oliveira <eduardobolive@gmail.com>
2023-01-11 20:48:02 -0300

    Exercicio 1 - Adicionando nome no header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,10 @@ type HeaderProps = {
     showModal():void
 }
 export const Header : NextPage<HeaderProps> = ({sair, showModal}) => {
+
+    var name = localStorage.getItem("name");
+    if(name == null) name = "~Erro~";
+
     return (
         <div className="container-header">
             <img src="/logo.svg" alt="Logo Fiap" className="logo"/>
@@ -14,7 +18,7 @@ export const Header : NextPage<HeaderProps> = ({sair, showModal}) => {
                 <img src="/exit-mobile.svg" alt="Sair" onClick={sair}/>
             </div>
             <div className="desktop">
-                <span>Olá, </span>
+                <span>Olá, {name}</span>
                 <img src="/exit-desktop.svg" alt="Sair" onClick={sair}/>
             </div>
         </div>
